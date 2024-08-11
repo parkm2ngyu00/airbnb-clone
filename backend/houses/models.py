17cdf7191c44e02f09b913c1b940bc4511bb5454
Mingyu Park <parkm2ngyu00@naver.com>
2024-08-11 11:23:06 +0900

    Modify models.py and admin.py

--- backend/houses/models.py
+++ backend/houses/models.py
@@ -5,7 +5,16 @@ class House(models.Model):
     """Model Definition for Houses"""
 
     name = models.CharField(max_length=30)
-    price_per_night = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Positive Numbers Only"
+    )
     description = models.TextField()
-    address = models.CharField(max_length=30)
-    pets_allowed = models.BooleanField(default=True)
+    address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(
+        verbose_name="Pets Allowed?",
+        default=True,
+        help_text="Does this house allow pets?",
+    )
+
+    def __str__(self):
+        return self.name
