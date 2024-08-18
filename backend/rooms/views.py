136ed446ce71af68483bb9dcd1885544406f2724
Mingyu Park <parkm2ngyu00@naver.com>
2024-08-18 21:37:24 +0900

    Add GET for rooms and room detail

--- backend/rooms/views.py
+++ backend/rooms/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Amenity
-from .serializers import AmenitySerializer
+from .models import Amenity, Room
+from .serializers import AmenitySerializer, RoomListSerializer, RoomDetailSerializer
 from rest_framework.exceptions import NotFound
 from rest_framework.status import HTTP_204_NO_CONTENT, HTTP_400_BAD_REQUEST
 
+
+class Rooms(APIView):
+
+    def get(self, request):
+        all_rooms = Room.objects.all()
+        serializer = RoomListSerializer(all_rooms, many=True)
+        return Response(serializer.data)
+    
+
+class RoomDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
+
+    def get(self, request, pk):
+        room = self.get_object(pk=pk)
+        serializer = RoomDetailSerializer(room)
+        return Response(serializer.data)
+    
+    
 class Amenities(APIView):
 
     def get(self, request):
