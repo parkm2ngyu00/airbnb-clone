901f25a33c6d1c0015354dbaf2e09d2617613880
Mingyu Park <parkm2ngyu00@naver.com>
2024-08-21 19:39:31 +0900

    Add Header

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,17 @@
+import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>
 );
