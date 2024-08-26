9a9a5bc7b606cfc95838f8da33dc7b26c51560c2
Mingyu Park <parkm2ngyu00@naver.com>
2024-08-26 22:01:28 +0900

    Add github login link

--- frontend/src/components/SocialLogin.tsx
+++ frontend/src/components/SocialLogin.tsx
@@ -12,7 +12,12 @@ export default function SocialLogin() {
         <Divider />
       </HStack>
       <VStack>
-        <Button w="100%" leftIcon={<FaGithub />} colorScheme={"telegram"}>
+        <Button
+          as="a"
+          href="https://github.com/login/oauth/authorize?client_id=Ov23liXrbV2BQ2sNqgI4&scope=read:user,user:email"
+          w="100%"
+          leftIcon={<FaGithub />}
+        >
           Continue with Github
         </Button>
         <Button w="100%" leftIcon={<FaComment />} colorScheme={"yellow"}>
